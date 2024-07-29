028529d514771f6b350b68ed230c296008d5d5c7
YuejiaoShi <shiyuejiaosyj@gmail.com>
2024-07-29 22:44:13 +0200

    Try to create an object type with type specification for inside key-values

---  TypeScript Basics/Basic_TS_App.ts
+++  TypeScript Basics/Basic_TS_App.ts
@@ -1,2 +1,18 @@
 let userName: string;
 userName = "Yuejiao";
+
+// let user: object;
+type User = {
+  name: string;
+  age: number;
+  isAdmin: boolean;
+  id: string | number;
+};
+
+let user: User;
+user = {
+  name: "Yuejiao",
+  age: 25,
+  isAdmin: true,
+  id: "abc12",
+};
