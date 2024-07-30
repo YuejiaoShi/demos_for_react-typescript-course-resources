3344537d74f69bebc49b02281ef78031456ff1c2
YuejiaoShi <shiyuejiaosyj@gmail.com>
2024-07-30 22:53:24 +0200

    create components folder, make CourseGoal comp; change Snippets for tsx files

---
+++ react-ts-basics-app/src/components/CourseGoal.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+interface Props {
+  // define props here
+}
+
+const CourseGoal: React.FC<Props> = ({  }) => {
+  return (
+    <div>
+      // component content
+    </div>
+  );
+}
+
+export default ComponentName;
