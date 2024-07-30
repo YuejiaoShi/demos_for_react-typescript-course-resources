9035cc40299cda04bdb19c20561d3bead899b6b9
YuejiaoShi <shiyuejiaosyj@gmail.com>
2024-07-30 23:33:51 +0200

    Create a Header component with props: image, children

--- react-ts-basics-app/src/App.tsx
+++ react-ts-basics-app/src/App.tsx
@@ -3,7 +3,10 @@ import CourseGoal from "./components/CourseGoal";
 export default function App() {
   return (
     <main>
-      <CourseGoal title='Learn react+ TS' description="Learn it from the ground up">
+      <CourseGoal
+        title="Learn react+ TS"
+        description="Learn it from the ground up"
+      >
         <p>what is your goal?</p>
       </CourseGoal>
     </main>
