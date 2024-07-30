9353255485afcf7a738db617ca235c66276c2276
YuejiaoShi <shiyuejiaosyj@gmail.com>
2024-07-30 23:36:13 +0200

    use Header in App and give the props' values

--- react-ts-basics-app/src/App.tsx
+++ react-ts-basics-app/src/App.tsx
@@ -1,8 +1,13 @@
 import CourseGoal from "./components/CourseGoal";
+import Header from "./components/Header";
+import goalImg from "./assets/goals.jpg";
 
 export default function App() {
   return (
     <main>
+      <Header image={{ src: goalImg, alt: "list a goals" }}>
+        <h1>Your Course Goals</h1>
+      </Header>
       <CourseGoal
         title="Learn react+ TS"
         description="Learn it from the ground up"
