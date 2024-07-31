5d396eb386e32936f85613c65e805c3abb4d1942
YuejiaoShi <shiyuejiaosyj@gmail.com>
2024-07-31 16:49:30 +0200

    add a button and a triggered function

--- react-ts-basics-app/src/App.tsx
+++ react-ts-basics-app/src/App.tsx
@@ -3,11 +3,13 @@ import Header from "./components/Header";
 import goalImg from "./assets/goals.jpg";
 
 export default function App() {
+  function handleAddGoal() {}
   return (
     <main>
       <Header image={{ src: goalImg, alt: "list a goals" }}>
         <h1>Your Course Goals</h1>
       </Header>
+      <button onClick={handleAddGoal}>Add Goal</button>
       <CourseGoal
         title="Learn react+ TS"
         description="Learn it from the ground up"
