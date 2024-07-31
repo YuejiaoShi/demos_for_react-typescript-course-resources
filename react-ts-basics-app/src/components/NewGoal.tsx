0161d42c83680f07b9eb6375acc2a8df2ba83928
YuejiaoShi <shiyuejiaosyj@gmail.com>
2024-07-31 17:52:23 +0200

    utilize useRef with initial value 'null' to receive the values

--- react-ts-basics-app/src/components/NewGoal.tsx
+++ react-ts-basics-app/src/components/NewGoal.tsx
@@ -1,10 +1,13 @@
-import React, { FormEvent } from "react";
+import React, { FormEvent, useRef } from "react";
 
 interface Props {
   // define props here
 }
 
 const NewGoal: React.FC<Props> = ({}) => {
+  const goal = useRef(null);
+  const summary = useRef(null);
+
   function handleSubmit(event: FormEvent<HTMLFormElement>) {
     event.preventDefault;
   }
@@ -12,11 +15,11 @@ const NewGoal: React.FC<Props> = ({}) => {
     <form onSubmit={handleSubmit}>
       <p>
         <label htmlFor="goal">Your Goal</label>
-        <input id="goal" type="text" name="goal"></input>
+        <input id="goal" type="text" name="goal" ref={goal}></input>
       </p>
       <p>
         <label htmlFor="summary">Short Summary</label>
-        <input id="summary" type="text"></input>
+        <input id="summary" type="text" ref={summary}></input>
       </p>
       <button>Add Goal</button>
     </form>
