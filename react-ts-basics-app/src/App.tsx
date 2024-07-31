2ce270f4c366deb8b7d1b47607367de819864947
YuejiaoShi <shiyuejiaosyj@gmail.com>
2024-07-31 16:54:54 +0200

    add a state, and a type CourseGoal was needed for defining the state. so the goals state is an array of CourseGoal type values

--- react-ts-basics-app/src/App.tsx
+++ react-ts-basics-app/src/App.tsx
@@ -1,9 +1,15 @@
 import CourseGoal from "./components/CourseGoal";
 import Header from "./components/Header";
 import goalImg from "./assets/goals.jpg";
+import { useState } from "react";
+
+type CourseGoal = { title: string; description: string; id: number };
 
 export default function App() {
-  function handleAddGoal() {}
+  const [goals, setGoals] = useState<CourseGoal[]>([]);
+  function handleAddGoal() {
+    setGoals([...goals]);
+  }
   return (
     <main>
       <Header image={{ src: goalImg, alt: "list a goals" }}>
