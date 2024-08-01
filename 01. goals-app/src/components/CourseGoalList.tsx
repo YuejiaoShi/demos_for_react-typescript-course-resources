5c372bd40829e20213ecca2fcd63e1cf924e8993
YuejiaoShi <shiyuejiaosyj@gmail.com>
2024-08-01 16:41:19 +0200

    conditionally render warningBox with warning mode InfoBox when goals.length >= 4

--- 01. goals-app/src/components/CourseGoalList.tsx
+++ 01. goals-app/src/components/CourseGoalList.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { ReactNode } from "react";
 import CourseGoal from "./CourseGoal.tsx";
 import { type CourseGoal as CourseGoalProps } from "../App.tsx";
 import InfoBox from "./Infobox.tsx";
@@ -19,21 +19,34 @@ const CourseGoalList: React.FC<CourseGoalListProps> = ({
       </InfoBox>
     );
   }
+
+  let warningBox: ReactNode;
+  if (goals.length >= 4) {
+    warningBox = (
+      <InfoBox mode="warning">
+        You are collecting a lot of goals. Don't put too much on your plate!
+      </InfoBox>
+    );
+  }
+
   return (
-    <ul>
-      {goals.map((goal) => (
-        <li key={goal.id}>
-          <CourseGoal
-            id={goal.id}
-            title={goal.title}
-            description={goal.description}
-            onDelete={onDeleteGoal}
-          >
-            <p>what is your goal?</p>
-          </CourseGoal>
-        </li>
-      ))}
-    </ul>
+    <>
+      {warningBox}
+      <ul>
+        {goals.map((goal) => (
+          <li key={goal.id}>
+            <CourseGoal
+              id={goal.id}
+              title={goal.title}
+              description={goal.description}
+              onDelete={onDeleteGoal}
+            >
+              <p>what is your goal?</p>
+            </CourseGoal>
+          </li>
+        ))}
+      </ul>
+    </>
   );
 };
 
