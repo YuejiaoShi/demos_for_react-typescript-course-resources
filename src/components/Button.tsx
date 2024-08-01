07c8896ec886f52942834f9b9607098ece8a9512
YuejiaoShi <shiyuejiaosyj@gmail.com>
2024-08-01 22:45:01 +0200

    never type for href oo ButtonProps; Optional string type for href oo AnchorProps

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,9 +1,10 @@
 import React, { type ComponentPropsWithoutRef } from "react";
 
-type ButtonProps = ComponentPropsWithoutRef<"button">;
+type ButtonProps = ComponentPropsWithoutRef<"button"> & { href: never };
 
-type AnchorProps = ComponentPropsWithoutRef<"a">;
+type AnchorProps = ComponentPropsWithoutRef<"a"> & { href?: string };
 
+// Type Predicates
 function isAnchorProp(props: ButtonProps | AnchorProps): props is AnchorProps {
   return "href" in props;
 }
