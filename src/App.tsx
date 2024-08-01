3a230db5960148f0999835bc342f4b2c0f21eb48
YuejiaoShi <shiyuejiaosyj@gmail.com>
2024-08-01 21:56:51 +0200

    add a anchor Button and a Link Button

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,18 @@
+import Button from "./components/Button";
 import Input from "./components/Input";
 
 function App() {
   return (
     <main>
+      <p>
+        <Button el="button">A Button</Button>
+      </p>
+      <p>
+        <Button el="anchor" href="https://google.com">
+          A Link
+        </Button>
+      </p>
+
       <Input id="name" label="Your name" type="text" />
       <Input id="age" label="Your age" type="number" />
     </main>
--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -10,8 +10,8 @@ type AnchorProps = {
 
 const Button: React.FC<ButtonProps | AnchorProps> = (props) => {
   //   const { el, ...otherProps } = props;
-  if (props.el === "anchor") return <a></a>;
-  return <button {...props}></button>;
+  if (props.el === "anchor") return <a {...props} className="button"></a>;
+  return <button {...props} className="button"></button>;
 };
 
 export default Button;
