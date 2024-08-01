a6947f5d5b3fcbb434b999c21811dfc503fe740b
YuejiaoShi <shiyuejiaosyj@gmail.com>
2024-08-01 16:43:28 +0200

    add one more InfoBoxProp severity: "low" | "medium" | "high"; to handle the different className

--- 01. goals-app/src/components/Infobox.tsx
+++ 01. goals-app/src/components/Infobox.tsx
@@ -3,9 +3,10 @@ import React, { ReactNode } from "react";
 interface InfoBoxProps {
   mode: "hint" | "warning";
   children: ReactNode;
+  severity: "low" | "medium" | "high";
 }
 
-const InfoBox: React.FC<InfoBoxProps> = ({ mode, children }) => {
+const InfoBox: React.FC<InfoBoxProps> = ({ mode, severity, children }) => {
   if (mode === "hint")
     return (
       <aside className="infobox infobox-hint">
@@ -14,7 +15,7 @@ const InfoBox: React.FC<InfoBoxProps> = ({ mode, children }) => {
     );
 
   return (
-    <aside className="infobox infobox-warning warning--medium">
+    <aside className={`infobox infobox-warning warning--${severity}`}>
       <h2>Warning</h2>
       <p>{children}</p>
     </aside>
