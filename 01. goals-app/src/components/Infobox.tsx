2a9c7a2ead746165a53891782283153e48af0bdf
YuejiaoShi <shiyuejiaosyj@gmail.com>
2024-08-01 16:55:53 +0200

    create InfoBoxProps to include both HintBoxProps | WarningBoxProps with different props in it and conditionally destructure and use the props in different conditions

--- 01. goals-app/src/components/Infobox.tsx
+++ 01. goals-app/src/components/Infobox.tsx
@@ -1,22 +1,36 @@
 import React, { ReactNode } from "react";
 
-
-interface InfoBoxProps {
-  mode: "hint" | "warning";
+type HintBoxProps = {
+  mode: "hint";
   children: ReactNode;
-  severity?: "low" | "medium" | "high";
-  // Another syntax for indicating an optional prop is:
-  // severity?: "low" | "medium" | "high" | undefined;
-}
+};
+type WarningBoxProps = {
+  mode: "warning";
+  children: ReactNode;
+  severity: "low" | "medium" | "high";
+};
 
-const InfoBox: React.FC<InfoBoxProps> = ({ mode, severity, children }) => {
-  if (mode === "hint")
+type InfoBoxProps = HintBoxProps | WarningBoxProps;
+
+// interface InfoBoxProps {
+//   mode: "hint" | "warning";
+//   children: ReactNode;
+//   severity?: "low" | "medium" | "high";
+//   // Another syntax for indicating an optional prop is:
+//   // severity?: "low" | "medium" | "high" | undefined;
+// }
+
+const InfoBox: React.FC<InfoBoxProps> = (props: InfoBoxProps) => {
+  const { children } = props;
+
+  if (props.mode === "hint") {
     return (
       <aside className="infobox infobox-hint">
         <p>{children}</p>
       </aside>
     );
-
+  }
+  const { severity } = props;
   return (
     <aside className={`infobox infobox-warning warning--${severity}`}>
       <h2>Warning</h2>
