bb8c0f77735bb2aa4e430631989c9e2256e17a2d
YuejiaoShi <shiyuejiaosyj@gmail.com>
2024-08-01 23:25:09 +0200

    Wrap Input into forwardRef to allow a ref input

--- src/components/Input.tsx
+++ src/components/Input.tsx
@@ -1,17 +1,19 @@
-import React, { ComponentPropsWithoutRef } from "react";
+import React, { ComponentPropsWithoutRef, forwardRef } from "react";
 
 type InputProps = {
   label: string;
   id: string;
 } & ComponentPropsWithoutRef<"input">;
 
-const Input: React.FC<InputProps> = ({ id, label, ...props }: InputProps) => {
-  return (
-    <p>
-      <label htmlFor={id}>{label}</label>
-      <input type="text" id={id} {...props} />
-    </p>
-  );
-};
+const Input = forwardRef<HTMLInputElement, InputProps>(
+  ({ id, label, ...props }, ref) => {
+    return (
+      <p>
+        <label htmlFor={id}>{label}</label>
+        <input type="text" id={id} {...props} ref={ref} />
+      </p>
+    );
+  }
+);
 
 export default Input;
