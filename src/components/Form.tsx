7555bd0a356bb7d4b0f4e7493f4c699b929bfe8c
YuejiaoShi <shiyuejiaosyj@gmail.com>
2024-08-01 23:42:17 +0200

    merge FormProps to a custom type

--- src/components/Form.tsx
+++ src/components/Form.tsx
@@ -1,13 +1,16 @@
 import React, { ComponentPropsWithoutRef, FormEvent } from "react";
 
-type FormProps = ComponentPropsWithoutRef<"form">;
+type FormProps = ComponentPropsWithoutRef<"form"> & {
+  onSave: (value: unknown) => void;
+};
 
 const Form: React.FC<FormProps> = (props) => {
   function handleSubmit(event: FormEvent<HTMLFormElement>) {
     event.preventDefault();
 
     const formData = new FormData(event.currentTarget);
-    const data = Object.formEntries(formData);
+    const data = Object.fromEntries(formData);
+    props.onSave(data);
   }
 
   return (
